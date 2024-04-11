bc0df8276cb853059681f886fa3e906489388fff
Fabrice OSSE <ossejoel@gmail.com>
2024-04-11 18:07:00 +0000

    code updated at 11042024

--- scripts.js
+++ scripts.js
@@ -93,16 +93,47 @@ function likeArt(i) {
     if(i == 1){
         dg = document.getElementById("likeArt"+i);
         dg.querySelector("svg").setAttribute("class", "fill-sky-900");
+        dg.querySelector("svg").removeAttribute("onclick");
+        dg.querySelector("svg").setAttribute("onclick", "unLikeArt(1)");
     }
 
     if(i == 2){
         dg = document.getElementById("likeArt"+i);
         dg.querySelector("svg").setAttribute("class", "fill-sky-900");
+        dg.querySelector("svg").removeAttribute("onclick");
+        dg.querySelector("svg").setAttribute("onclick", "unLikeArt(2)");
     }
     
     if(i == 3){
         dg = document.getElementById("likeArt"+i);
         dg.querySelector("svg").setAttribute("class", "fill-sky-900");
+        dg.querySelector("svg").removeAttribute("onclick");
+        dg.querySelector("svg").setAttribute("onclick", "unLikeArt(3)");
+    }
+}
+
+function unLikeArt(i) {
+
+    var dg;
+    if(i == 1){
+        dg = document.getElementById("likeArt"+i);
+        dg.querySelector("svg").removeAttribute("class");
+        dg.querySelector("svg").removeAttribute("onclick");
+        dg.querySelector("svg").setAttribute("onclick", "likeArt(1)");
+    }
+
+    if(i == 2){
+        dg = document.getElementById("likeArt"+i);
+        dg.querySelector("svg").removeAttribute("class");
+        dg.querySelector("svg").removeAttribute("onclick");
+        dg.querySelector("svg").setAttribute("onclick", "likeArt(2)");
+    }
+
+    if(i == 3){
+        dg = document.getElementById("likeArt"+i);
+        dg.querySelector("svg").removeAttribute("class");
+        dg.querySelector("svg").removeAttribute("onclick");
+        dg.querySelector("svg").setAttribute("onclick", "likeArt(3)");
     }
 }
 
@@ -110,25 +141,42 @@ function removeArt(i){
 
     var dg;
     var totalOrder;
+    var confirmation;
+
     if(i == 1){
-        dg = document.getElementById("detailArt"+i);
-        dg.remove();
-        
-        totalOrder = parseInt(document.getElementById("total").textContent);
-        document.getElementById("total").textContent = totalOrder - 25000;
+
+        confirmation = confirm("Voulez-vous vraiment supprimer l'article ?");
+
+        if(confirmation){
+
+            dg = document.getElementById("detailArt"+i);
+            dg.remove();
+            totalOrder = parseInt(document.getElementById("total").textContent);
+            document.getElementById("total").textContent = totalOrder - 25000;
+        }
     }
     if(i == 2){
-        dg = document.getElementById("detailArt"+i);
-        dg.remove();
-        
-        totalOrder = parseInt(document.getElementById("total").textContent);
-        document.getElementById("total").textContent = totalOrder - 40000;
+
+        confirmation = confirm("Voulez-vous vraiment supprimer l'article ?");
+
+        if(confirmation){
+
+            dg = document.getElementById("detailArt"+i);
+            dg.remove();
+            totalOrder = parseInt(document.getElementById("total").textContent);
+            document.getElementById("total").textContent = totalOrder - 40000;
+        }
     }
     if(i == 3){
-        dg = document.getElementById("detailArt"+i);
-        dg.remove();
-        
-        totalOrder = parseInt(document.getElementById("total").textContent);
-        document.getElementById("total").textContent = totalOrder - 18500;
+
+        confirmation = confirm("Voulez-vous vraiment supprimer l'article ?");
+
+        if(confirmation){
+
+            dg = document.getElementById("detailArt"+i);
+            dg.remove();
+            totalOrder = parseInt(document.getElementById("total").textContent);
+            document.getElementById("total").textContent = totalOrder - 18500;
+        }
     }
 }
